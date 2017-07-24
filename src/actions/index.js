c53287d095607ad4f1f72e863f2f14e74d6bed19
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-07-24 16:46:59 +0300

    Add actions, reducer, initial PostsIndex component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,38 @@
 import axios from 'axios';
 
-const API_KEY = 'xquader';
+const API_KEY = '?key=xquader';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
-export function fetchWeather(city) {
+export function fetchPosts() {
+    const url = `${ROOT_URL}/posts${API_KEY}`;
+    const request = axios.get(url);
 
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    }
+}
+
+export function fetchPost(postId) {
+    const url = `${ROOT_URL}/posts/${postId}${API_KEY}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    }
+}
+
+export function deletePost(postId) {
+    const url = `${ROOT_URL}/posts/${postId}${API_KEY}`;
+    const request = axios.get(url);
+
+    return {
+        type: DELETE_POST,
+        payload: request
+    }
 }
---
+++ src/reducers/PostsReducer.js
@@ -0,0 +1,13 @@
+import {FETCH_POSTS} from "../actions/index";
+import _ from 'underscore';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            return _.reduce(action.payload.data, (memo, item) => {
+                memo[item.id] = _.omit(item, 'id');
+            }, {});
+        default:
+            return state;
+    }
+};
