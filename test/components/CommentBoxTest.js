858ab36857b4501823a225e9845bcceca7ac97f2
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-07-28 17:01:10 +0300

    Add beforeEach for CommentBox tests

--- test/components/CommentBoxTest.js
+++ test/components/CommentBoxTest.js
@@ -2,22 +2,21 @@ import {renderComponent, expect} from '../test_helper';
 import CommentBox from '../../src/components/CommentBox';
 
 describe('CommentBox', () => {
+    let component;
+
+    beforeEach(() => {
+        component = renderComponent(CommentBox);
+    });
 
     it('has correct class', () => {
-        const component = renderComponent(CommentBox);
-
         expect(component).has.class('comment-box');
     });
 
     it('has textarea', () => {
-        const component = renderComponent(CommentBox);
-
         expect(component.find('textarea')).to.exist;
     });
 
     it('has button', () => {
-        const component = renderComponent(CommentBox);
-
         expect(component.find('button')).to.exist;
     });
 
