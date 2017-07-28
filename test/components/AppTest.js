431f5db3bed2ffdcbfa8783866abf5689f51e26a
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-07-28 16:51:40 +0300

    Initial CommentBox component and tests for it

--- test/components/AppTest.js
+++ test/components/AppTest.js
@@ -1,7 +1,7 @@
 import {renderComponent, expect} from '../test_helper';
 import App from '../../src/components/App';
 
-describe('App testing', () => {
+describe('App', () => {
 
     it('shows the correct text', () => {
         const component = renderComponent(App);
