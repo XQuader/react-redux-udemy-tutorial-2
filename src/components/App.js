2de07a23d1debb7f8c7441cd2fbbdecfe2f0770a
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-07-28 15:00:47 +0300

    Remove previous tutorial files

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,11 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
-import PostsIndex from './PostsIndex';
-import PostsNew from './PostsNew';
-import PostsShow from './PostsShow';
-
 
 import '../../style/style.less'
 
 export default class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <div>
-                    <Switch>
-                        <Route path="/posts/new" component={PostsNew}/>
-                        <Route path="/posts/:id" component={PostsShow}/>
-                        <Route path="/" component={PostsIndex}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
+            <div>React Simple Starter</div>
         )
     }
 }
