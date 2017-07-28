30dfd82293e46b486aeb53782be090e7d23ffe2f
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-07-28 16:01:21 +0300

    Create first test

--- test/components/AppTest.js
+++ test/components/AppTest.js
@@ -1,1 +1,11 @@
-import React, {Component} from 'react';
+import {renderComponent, expect} from '../test_helper';
+import App from '../../src/components/App';
+
+describe('App testing', () => {
+
+    it('shows the correct text', () => {
+        const component = renderComponent(App);
+
+        expect(component).to.contain('React simple starter');
+    })
+});
