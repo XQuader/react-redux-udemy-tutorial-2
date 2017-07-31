9b918f42c70551e9bec1e3366cb675bc0e8a0231
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-07-31 11:41:02 +0300

    AppTest checks for .comment-box existence

--- test/components/AppTest.js
+++ test/components/AppTest.js
@@ -2,10 +2,13 @@ import {renderComponent, expect} from '../test_helper';
 import App from '../../src/components/App';
 
 describe('App', () => {
+    let component;
 
-    it('shows the correct text', () => {
-        const component = renderComponent(App);
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
 
-        expect(component).to.contain('React simple starter');
+    it('shows a comment box', () => {
+        expect(component.find('.comment-box')).to.exist;
     })
 });
