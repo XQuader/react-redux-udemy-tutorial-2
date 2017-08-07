d5da4599ea3b6b2b0759a14ab17d87141700607f
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-08-07 18:06:18 +0300

    Send jwt token on signup. Add jwtLogin method

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,14 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = Date.now();
+    return jwt.encode({
+        sub: user.id,
+        iat: timestamp
+    }, config.secret);
+}
 
 exports.signup = ({body: {email, password}}, res, next) => {
     User.findOne({email}, (err, existingUser) => {
@@ -8,19 +18,15 @@ exports.signup = ({body: {email, password}}, res, next) => {
             return res.status(422).send({error: 'Email is in use'});
         }
 
-        try{
-            const user = new User({email, password});
+        const user = new User({email, password});
 
-            user.save((err) => {
-                if (err) {
-                    res.status(401).send({error: 'Bad request'});
-                    return next(err);
-                }
+        user.save((err) => {
+            if (err) {
+                res.status(401).send({error: 'Bad request'});
+                return next(err);
+            }
 
-                res.json({success: true});
-            });
-        } catch (err) {
-
-        }
+            res.json({token: tokenForUser(user)});
+        });
     });
 };
